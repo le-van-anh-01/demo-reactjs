0503a59bce8a551157ff81e1f74aa5132771d8c8
le-van-anh-01 <levananh@morien.com>
2021-08-24 16:47:03 +0900

    Create components for LifeCyles

---
+++ src/component/LifeCycle/components/Count/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper } from './styles';
+
+const Count = () => {
+    return (
+        <Wrapper>
+            <h1>This is Count</h1>
+        </Wrapper>
+    )
+}
+
+export default Count;
---
+++ src/component/LifeCycle/components/Notice/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper } from './styles';
+
+const Notice = () => {
+    return (
+        <Wrapper>
+            <h1>This is Notice</h1>
+        </Wrapper>
+    )
+}
+
+export default Notice
